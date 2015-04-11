5bb832863cb79418dc47b6189a3f9a2c2a275e87
ericdouglas <eric.douglas.mail@gmail.com>
2015-04-11 10:54:03 -0300

    Create the RoomController

---
+++ client/room/room.controller.js
@@ -0,0 +1,25 @@
+(function() {
+
+  'use strict';
+
+  angular
+    .module( 'blackjack' )
+    .controller( 'RoomController', RoomController );
+
+  RoomController.$inject = [
+    'RoomService'
+  ];
+
+  function RoomController( RoomService ) {
+  
+    ///////////// Properties
+
+    ///////////// Public Methods
+
+    ///////////// Private Methods
+
+    ///////////// Methods Declaration
+  
+  }
+
+}());
