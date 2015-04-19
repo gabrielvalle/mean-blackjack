ec463a35644f7fc8ac46711ca776a0759cb2bab3
ericdouglas <eric.douglas.mail@gmail.com>
2015-04-19 10:00:46 -0300

    Test give a card to a player

--- test/client/helpers/dealer.service.test.js
+++ test/client/helpers/dealer.service.test.js
@@ -6,8 +6,9 @@ describe( 'DealerService', function() {
 
     module( 'blackjack' );
 
-    inject( function( _DealerService_ ) {
+    inject( function( _DealerService_, _RoomService_ ) {
       DealerService = _DealerService_;
+      RoomService   = _RoomService_;
     });
 
   });
@@ -23,6 +24,21 @@ describe( 'DealerService', function() {
 
       expect( deck ).to.have.length( 52 );
       expect( deck[ 0 ]).to.eql( card );
+
+    });
+
+  });
+
+  describe( '#giveCard()', function() {
+
+    it( 'should give one card to a player', function() {
+
+      RoomService.createPlayers( 3 );
+      DealerService.createDeck();
+      
+      DealerService.giveCard( DealerService.length, RoomService.playersData[ 0 ]);
+
+      expect( RoomService.playersData[ 0 ].hand ).to.have.length( 1 );
 
     });
 
