632982fb2f01ef4f1cecc0f0f0ad576145f8a0ad
ericdouglas <eric.douglas.mail@gmail.com>
2015-04-18 22:33:43 -0300

    Add test to home.controller -> function that add and remove a player

---
+++ test/client/home/home.controller.test.js
@@ -0,0 +1,36 @@
+var expect = chai.expect;
+
+describe( 'HomeController', function() {
+
+  beforeEach( module( 'blackjack' ));
+  beforeEach(module( 'stateMock' ));
+
+  describe( '#addRemovePlayer()', function() {
+
+    beforeEach( inject( function( $controller ) {
+
+      ctrl  = $controller( 'HomeController' );
+
+    }));
+
+    it( 'should add 1 in totalPlayers when the parameter is equals to 1', function() {
+
+      ctrl.totalPlayers = 1;
+      ctrl.addRemovePlayer( 1 );
+
+      expect( ctrl.totalPlayers ).to.equal( 2 );
+
+    });
+
+    it( 'should remove 1 in totalPlayers when the parameter is equals to -1', function() {
+
+      ctrl.totalPlayers = 2;
+      ctrl.addRemovePlayer( -1 );
+
+      expect( ctrl.totalPlayers ).to.equal( 1 );
+
+    });
+
+  });
+
+});
